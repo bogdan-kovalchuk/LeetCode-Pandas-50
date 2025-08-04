33d7e38ed75233466790db42859353d4c9ad0c8d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-04 14:58:03 +0300

    626 solution

--- Subqueries/626. Exchange Seats.py
+++ Subqueries/626. Exchange Seats.py
@@ -4,7 +4,10 @@ import pandas as pd
 def exchange_seats(seat: pd.DataFrame) -> pd.DataFrame:
     temp = seat["student"].copy()
 
-    seat.loc[seat.index % 2 == 0, "student"] = temp.shift(-1)[seat.index % 2 == 0]
-    seat.loc[seat.index % 2 == 1, "student"] = temp.shift(1)[seat.index % 2 == 1]
+    seat.loc[::2, "student"] = temp.shift(-1)[::2]
+    seat.loc[1::2, "student"] = temp.shift(1)[1::2]
 
-    return seat.sort_values("id")
+    if seat.shape[0] % 2 != 0:
+        seat.loc[seat.index[-1], "student"] = temp.iloc[-1]
+
+    return seat
