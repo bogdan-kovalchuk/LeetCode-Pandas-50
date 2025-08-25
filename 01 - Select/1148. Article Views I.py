b6ec20376aaf3f938c6de1cf9bb49cb7cb7e855c
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-25 13:38:52 +0300

    1148 solution

--- 01 - Select/1148. Article Views I.py
+++ 01 - Select/1148. Article Views I.py
@@ -1,1 +1,11 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    result = (
+        views[views["author_id"] == views["viewer_id"]][["author_id"]]
+        .drop_duplicates()
+        .rename(columns={"author_id": "id"})
+        .sort_values(by="id", ascending=True)
+    )
+    return result
