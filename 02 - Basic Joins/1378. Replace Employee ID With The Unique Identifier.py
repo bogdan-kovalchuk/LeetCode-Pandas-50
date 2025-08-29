62a79a45e0d2441363da0bd9d4664a885f659db0
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-29 11:48:55 +0300

    1378 solution

--- 02 - Basic Joins/1378. Replace Employee ID With The Unique Identifier.py
+++ 02 - Basic Joins/1378. Replace Employee ID With The Unique Identifier.py
@@ -1,1 +1,5 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    return employees.merge(employee_uni, how="left", on="id")[["unique_id", "name"]]
