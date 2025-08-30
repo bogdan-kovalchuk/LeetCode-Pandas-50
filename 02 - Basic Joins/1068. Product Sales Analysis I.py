5f9d22f07b737ac89264fc64dd2775eaa2455a45
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-30 20:41:58 +0300

    1068 solution

--- 02 - Basic Joins/1068. Product Sales Analysis I.py
+++ 02 - Basic Joins/1068. Product Sales Analysis I.py
@@ -1,1 +1,7 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    return sales.merge(product, how="left", on="product_id")[
+        ["product_name", "year", "price"]
+    ]
