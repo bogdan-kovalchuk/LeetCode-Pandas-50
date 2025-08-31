b2bbfd380eea96e034624ea013068df1b4e693ba
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-31 16:18:53 +0300

    1581 solution

--- 02 - Basic Joins/1581. Customer Who Visited but Did Not Make Any Transactions.py
+++ 02 - Basic Joins/1581. Customer Who Visited but Did Not Make Any Transactions.py
@@ -1,1 +1,7 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
+    merged = visits.merge(transactions, how="left", on="visit_id")
+    no_trans = merged[merged["transaction_id"].isna()]
+    return no_trans.groupby("customer_id").size().reset_index(name="count_no_trans")
