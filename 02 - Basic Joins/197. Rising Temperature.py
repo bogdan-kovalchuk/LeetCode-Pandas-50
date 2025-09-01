d9327d37fa71079171e49c4f02cf29b0bd597ea6
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-01 10:34:54 +0300

    197 solution

--- 02 - Basic Joins/197. Rising Temperature.py
+++ 02 - Basic Joins/197. Rising Temperature.py
@@ -1,1 +1,9 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    weather = weather.sort_values(by="recordDate")
+    return weather[
+        (weather["temperature"].diff() > 0)
+        & (weather["recordDate"].diff().dt.days == 1)
+    ][["id"]]
