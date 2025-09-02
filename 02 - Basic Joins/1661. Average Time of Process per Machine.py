cff482cd861960bedaa21d2d442ef145c6f3ba99
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-02 15:38:14 +0300

    1661 solution

--- 02 - Basic Joins/1661. Average Time of Process per Machine.py
+++ 02 - Basic Joins/1661. Average Time of Process per Machine.py
@@ -1,1 +1,6 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+def get_average_time(activity: pd.DataFrame) -> pd.DataFrame:
+    pivot = pd.pivot_table(activity, values='timestamp', index=['machine_id', 'process_id'], columns=['activity_type']).reset_index()
+    pivot['processing_time'] = pivot['end'] - pivot['start']
+    return pivot.groupby('machine_id', as_index=False)['processing_time'].mean()[['machine_id', 'processing_time']].round(3)
