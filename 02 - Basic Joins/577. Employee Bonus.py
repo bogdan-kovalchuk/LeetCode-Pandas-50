b68df1d33ec97bd0982a6f2a32a54b857ff50f99
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-03 10:18:13 +0300

    577 solution

--- 02 - Basic Joins/577. Employee Bonus.py
+++ 02 - Basic Joins/577. Employee Bonus.py
@@ -1,1 +1,7 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    merged = employee.merge(bonus, how="left", on="empId")
+    print(merged)
+    return merged[(merged["bonus"].isna()) | (merged["bonus"] < 1000)][["name", "bonus"]]
