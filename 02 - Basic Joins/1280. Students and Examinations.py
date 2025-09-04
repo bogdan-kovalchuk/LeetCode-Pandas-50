36a48be1f2166e280bd93be116057165c597bbb9
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-04 09:09:17 +0300

    1280 solution

--- 02 - Basic Joins/1280. Students and Examinations.py
+++ 02 - Basic Joins/1280. Students and Examinations.py
@@ -1,1 +1,11 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def students_and_examinations(
+    students: pd.DataFrame, subjects: pd.DataFrame, examinations: pd.DataFrame
+) -> pd.DataFrame:
+    cross = students.merge(subjects, how="cross")
+    attended_exams = examinations.groupby(["student_id", "subject_name"]).size().reset_index(name="attended_exams")
+    result = cross.merge(attended_exams, how="left", on=["student_id", "subject_name"])
+    result["attended_exams"] = result["attended_exams"].fillna(0)
+    return result.sort_values(by=["student_id", "subject_name"])
