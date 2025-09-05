82b66cb091fdec4ba4de166e1d9faa529b86d3c3
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-05 14:56:08 +0300

    620 solution

--- 03 - Basic Aggregate Functions/620. Not Boring Movies.py
+++ 03 - Basic Aggregate Functions/620. Not Boring Movies.py
@@ -1,1 +1,7 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
+    return cinema[
+        (cinema["description"] != "boring") & (cinema["id"] % 2 != 0)
+    ].sort_values(by="rating", ascending=False)
