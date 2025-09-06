61e80a62ef39004ef846fa70e5629b85c5161a69
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-06 09:47:10 +0300

    570 solution

--- 02 - Basic Joins/570. Managers with at Least 5 Direct Reports.py
+++ 02 - Basic Joins/570. Managers with at Least 5 Direct Reports.py
@@ -1,1 +1,7 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    managers = employee.groupby("managerId").size().reset_index(name="count")
+    managers = managers[managers["count"] >= 5]["managerId"]
+    return employee[employee["id"].isin(managers)][["name"]]
