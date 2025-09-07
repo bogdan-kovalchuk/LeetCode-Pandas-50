253c60eec79dbf99f4d67b3c8d598f67899408de
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-07 21:19:08 +0300

    1075 solution

--- 03 - Basic Aggregate Functions/1075. Project Employees I.py
+++ 03 - Basic Aggregate Functions/1075. Project Employees I.py
@@ -1,1 +1,12 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+    merged = project.merge(employee, how="left", on="employee_id")
+    return (
+        merged.groupby("project_id")["experience_years"]
+        .mean()
+        .round(2)
+        .reset_index()
+        .rename(columns={"experience_years": "average_years"})
+    )
