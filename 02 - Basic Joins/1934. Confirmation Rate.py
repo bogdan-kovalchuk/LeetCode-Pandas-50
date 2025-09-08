326ec287660ef8eecd9d717d7b8589341baeb9cb
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-08 09:46:39 +0300

    1934 solution

--- 02 - Basic Joins/1934. Confirmation Rate.py
+++ 02 - Basic Joins/1934. Confirmation Rate.py
@@ -1,1 +1,10 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+def confirmation_rate(signups: pd.DataFrame, confirmations: pd.DataFrame) -> pd.DataFrame:
+    merged = signups.merge(confirmations, on="user_id", how="left")
+    merged["confirmation_rate"] = (merged["action"] == "confirmed").astype(int)
+    return (
+        merged.groupby("user_id", as_index=False)["confirmation_rate"]
+        .mean()
+        .round(2)
+    )
