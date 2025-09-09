12865498ad8471b38402215c8b7c74c2ab45e5f3
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-09 14:21:22 +0300

    1633 solution

--- 03 - Basic Aggregate Functions/1633. Percentage of Users Attended a Contest.py
+++ 03 - Basic Aggregate Functions/1633. Percentage of Users Attended a Contest.py
@@ -1,1 +1,9 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def users_percentage(users: pd.DataFrame, register: pd.DataFrame) -> pd.DataFrame:
+    g = register.groupby("contest_id")["user_id"].nunique().reset_index(name="unique_users")
+
+    g["percentage"] = (100 * g["unique_users"] / users["user_id"].nunique()).round(2)
+
+    return g[["contest_id", "percentage"]].sort_values(by=["percentage", "contest_id"], ascending=[False, True])
