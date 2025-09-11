a9f0b925dead8d48e38c56468cf04084624bc011
Bogdan Kovalchuk <bogdankovalchuk@gmail.com>
2025-09-11 18:46:33 +0300

    1211 solution

--- 03 - Basic Aggregate Functions/1211. Queries Quality and Percentage.py
+++ 03 - Basic Aggregate Functions/1211. Queries Quality and Percentage.py
@@ -1,1 +1,10 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+round2 = lambda x: round(x + 1e-9, 2)
+
+
+def queries_stats(queries: pd.DataFrame) -> pd.DataFrame:
+    queries["quality"] = queries.rating / queries.position
+    queries["poor_query_percentage"] = (queries.rating < 3) * 100
+    result = queries.groupby("query_name")[["quality", "poor_query_percentage"]].mean()
+    return result.apply(round2).reset_index()
