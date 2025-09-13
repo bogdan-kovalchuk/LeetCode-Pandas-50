35aa5af79effc9a1e88d44023771bad08cba4905
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-13 16:53:49 +0300

    2356 solution

--- 04 - Sorting and Grouping/2356. Number of Unique Subjects Taught by Each Teacher.py
+++ 04 - Sorting and Grouping/2356. Number of Unique Subjects Taught by Each Teacher.py
@@ -1,1 +1,7 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    return teacher.groupby("teacher_id", as_index=False).agg(
+        cnt=("subject_id", "nunique")
+    )[["teacher_id", "cnt"]]
