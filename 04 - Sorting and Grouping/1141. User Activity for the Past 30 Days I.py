bf145c984fc1f2e6b1cd9aa2391c147fb4c60811
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-14 19:59:50 +0300

    1141 solution

--- 04 - Sorting and Grouping/1141. User Activity for the Past 30 Days I.py
+++ 04 - Sorting and Grouping/1141. User Activity for the Past 30 Days I.py
@@ -1,1 +1,12 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def user_activity(activity: pd.DataFrame) -> pd.DataFrame:
+    activity = activity[activity["activity_date"].between("2019-06-28", "2019-07-27")]
+
+    return (
+        activity.groupby("activity_date")["user_id"]
+        .nunique()
+        .reset_index(name="active_users")
+        .rename(columns={"activity_date": "day"})
+    )
