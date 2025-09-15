cc0c29704942492c4de417c18a62313853e0510a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-15 09:57:38 +0300

    1251 solution

--- 03 - Basic Aggregate Functions/1251. Average Selling Price.py
+++ 03 - Basic Aggregate Functions/1251. Average Selling Price.py
@@ -1,1 +1,11 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def average_selling_price(prices: pd.DataFrame, units_sold: pd.DataFrame) -> pd.DataFrame:
+    merged = units_sold.merge(prices, on="product_id", how="left")
+    merged = merged[(merged["purchase_date"] >= merged["start_date"]) & (merged["purchase_date"] <= merged["end_date"])]
+    merged["revenue"] = merged["units"] * merged["price"]
+    grouped = merged.groupby("product_id")[["revenue", "units"]].sum().reset_index()
+    grouped["average_price"] = (grouped["revenue"] / grouped["units"]).round(2)
+    all_units = prices[["product_id"]].drop_duplicates().merge(grouped, how="left")
+    return all_units[["product_id", "average_price"]].fillna(0)
