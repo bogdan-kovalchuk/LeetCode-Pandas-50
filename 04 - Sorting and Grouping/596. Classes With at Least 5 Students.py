30b17e88008758887834de58b419db6a0a15e632
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-27 16:31:51 +0300

    596 solution

--- 04 - Sorting and Grouping/596. Classes With at Least 5 Students.py
+++ 04 - Sorting and Grouping/596. Classes With at Least 5 Students.py
@@ -1,1 +1,7 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    g = courses.groupby("class").size()
+    g = g[g >= 5]
+    return g.reset_index()[["class"]]
