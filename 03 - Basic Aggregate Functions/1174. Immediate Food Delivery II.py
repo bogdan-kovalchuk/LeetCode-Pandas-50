f48b73493f484178fa7e73406f02b68f78123e91
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-29 14:47:08 +0300

    1174 solution

--- 03 - Basic Aggregate Functions/1174. Immediate Food Delivery II.py
+++ 03 - Basic Aggregate Functions/1174. Immediate Food Delivery II.py
@@ -1,1 +1,9 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+import numpy as np
+
+
+def immediate_food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    delivery = delivery.sort_values(by="order_date")
+    delivery = delivery.drop_duplicates("customer_id")
+    delivery["order_type"] = np.where(delivery["order_date"] == delivery["customer_pref_delivery_date"], 1, 0)
+    return pd.DataFrame({"immediate_percentage": [np.round(100 * delivery["order_type"].mean(), 2)]})
