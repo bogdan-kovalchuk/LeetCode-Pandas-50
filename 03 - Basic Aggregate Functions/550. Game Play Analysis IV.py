984844a2c33520ff99bc041113fce6b2046e541f
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-09-30 08:50:51 +0300

    550 solution

--- 03 - Basic Aggregate Functions/550. Game Play Analysis IV.py
+++ 03 - Basic Aggregate Functions/550. Game Play Analysis IV.py
@@ -1,1 +1,6 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+def gameplay_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    activity["first_day"] = activity.groupby("player_id")["event_date"].transform(min)
+    second_day = activity[activity["first_day"] + pd.DateOffset(1) == activity["event_date"]]
+    return pd.DataFrame({"fraction": [round(len(second_day) / activity.player_id.nunique(), 2)]})
