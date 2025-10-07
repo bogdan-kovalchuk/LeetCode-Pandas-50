c7e7d917423d81118acbe416d7a5e873dc219edd
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-10-07 12:40:11 +0300

    1070 solution

--- 04 - Sorting and Grouping/1070. Product Sales Analysis III.py
+++ 04 - Sorting and Grouping/1070. Product Sales Analysis III.py
@@ -1,1 +1,6 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame) -> pd.DataFrame:
+    sales["first_year"] = sales.groupby("product_id")["year"].transform("min")
+    sales = sales[sales["year"] == sales["first_year"]]
+    return sales[["product_id", "first_year", "quantity", "price"]]
