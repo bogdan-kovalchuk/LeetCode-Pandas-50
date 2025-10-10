8350c8118c78ad8258ceb3bb412a14a699b85ed1
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-10-10 13:13:18 +0300

    1729 solution

--- 04 - Sorting and Grouping/1729. Find Followers Count.py
+++ 04 - Sorting and Grouping/1729. Find Followers Count.py
@@ -1,1 +1,9 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def count_followers(followers: pd.DataFrame) -> pd.DataFrame:
+    return (
+        followers.groupby("user_id", as_index=False)
+        .agg(followers_count=("follower_id", "count"))
+        .sort_values(by="user_id", ascending=True)
+    )
