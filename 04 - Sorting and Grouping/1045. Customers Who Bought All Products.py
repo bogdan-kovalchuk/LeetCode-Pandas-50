1c09cdc44ffa1368bae21904cdedfb5360f7bf41
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-10-12 10:03:37 +0300

    1045 solution

--- 04 - Sorting and Grouping/1045. Customers Who Bought All Products.py
+++ 04 - Sorting and Grouping/1045. Customers Who Bought All Products.py
@@ -1,1 +1,7 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def find_customers(customer: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    n_products = product.size
+    g = customer.groupby("customer_id")["product_key"].nunique().reset_index()
+    return g[g["product_key"] == n_products][["customer_id"]]
