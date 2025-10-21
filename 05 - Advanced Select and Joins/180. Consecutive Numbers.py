22e19ddfca669cd0a84bfe810c2b29ce7676cd11
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-10-21 09:50:13 +0300

    180 solution

--- 05 - Advanced Select and Joins/180. Consecutive Numbers.py
+++ 05 - Advanced Select and Joins/180. Consecutive Numbers.py
@@ -1,1 +1,6 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+def consecutive_numbers(logs: pd.DataFrame) -> pd.DataFrame:
+    logs['consecutive'] = (logs['num'] == logs['num'].shift(1)) & (logs['num'] == logs['num'].shift(2))
+    return logs[logs['consecutive']][['num']].rename(columns={"num": "ConsecutiveNums"}).drop_duplicates()
+    
