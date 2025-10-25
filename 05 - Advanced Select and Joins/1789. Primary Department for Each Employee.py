c6f3a982caf16b699beba21f174dea42ce3446c4
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-10-25 16:07:52 +0300

    1789 solution

--- 05 - Advanced Select and Joins/1789. Primary Department for Each Employee.py
+++ 05 - Advanced Select and Joins/1789. Primary Department for Each Employee.py
@@ -1,1 +1,8 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def find_primary_department(employee: pd.DataFrame) -> pd.DataFrame:
+    employee["entry_count"] = employee.groupby("employee_id")["employee_id"].transform("count")
+    return employee[(employee["primary_flag"] == "Y") | (employee["entry_count"] == 1)][
+        ["employee_id", "department_id"]
+    ]
