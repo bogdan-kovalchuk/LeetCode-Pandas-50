c2ccd989af6ee836a58b3e479dd9cd98cfa0c350
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-10-27 08:52:12 +0200

    610 solution

--- 05 - Advanced Select and Joins/610. Triangle Judgement.py
+++ 05 - Advanced Select and Joins/610. Triangle Judgement.py
@@ -1,1 +1,11 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def triangle_judgement(triangle: pd.DataFrame) -> pd.DataFrame:
+    triangle["triangle"] = (
+        (triangle["x"] + triangle["y"] > triangle["z"])
+        & (triangle["x"] + triangle["z"] > triangle["y"])
+        & (triangle["z"] + triangle["y"] > triangle["x"])
+    )
+    triangle["triangle"] = triangle["triangle"].map({True: "Yes", False: "No"})
+    return triangle
