0976ea0826d1a728e56ab268d08c5f256324db33
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-11-03 12:42:20 +0200

    1978 solution

--- 06 - Subqueries/1978. Employees Whose Manager Left the Company.py
+++ 06 - Subqueries/1978. Employees Whose Manager Left the Company.py
@@ -1,1 +1,7 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def find_employees(employees: pd.DataFrame) -> pd.DataFrame:
+    employees_less_30000 = employees[employees["salary"] < 30000].dropna()
+    result = employees_less_30000[~employees_less_30000["manager_id"].isin(employees["employee_id"])]
+    return result[["employee_id"]].sort_values(by="employee_id")
