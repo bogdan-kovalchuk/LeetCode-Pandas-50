cfdeb156e35d1aa2ea93bbec4c65a944930f98b3
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-11-07 08:52:03 +0200

    1204 solution

--- 05 - Advanced Select and Joins/1204. Last Person to Fit in the Bus.py
+++ 05 - Advanced Select and Joins/1204. Last Person to Fit in the Bus.py
@@ -1,1 +1,8 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def last_passenger(queue: pd.DataFrame) -> pd.DataFrame:
+    queue = queue.sort_values(by="turn")
+    queue["cumsum"] = queue["weight"].cumsum()
+    person_name = queue[queue["cumsum"] <= 1000].tail(1)["person_name"]
+    return pd.DataFrame({"person_name": person_name})
