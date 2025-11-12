9ff54441e6a2e7e5a771b0df95da07e7410a1dea
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-11-12 15:09:40 +0200

    626 solution

--- 06 - Subqueries/626. Exchange Seats.py
+++ 06 - Subqueries/626. Exchange Seats.py
@@ -1,1 +1,10 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def exchange_seats(seat: pd.DataFrame) -> pd.DataFrame:
+    temp = seat.copy()
+    seat.loc[::2, "student"] = temp["student"].shift(-1)
+    seat.loc[1::2, "student"] = temp["student"].shift(1)
+    if seat.shape[0] % 2 != 0:
+        seat.iloc[-1] = temp.iloc[-1]
+    return seat
