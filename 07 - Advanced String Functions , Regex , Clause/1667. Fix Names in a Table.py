6ad97da5c0283ffaf45fcd6905fd02eabb9f5150
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-11-14 11:33:23 +0200

    1667 solution

--- 07 - Advanced String Functions , Regex , Clause/1667. Fix Names in a Table.py
+++ 07 - Advanced String Functions , Regex , Clause/1667. Fix Names in a Table.py
@@ -1,1 +1,6 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users["name"] = users["name"].str.capitalize()
+    return users.sort_values(by="user_id")
