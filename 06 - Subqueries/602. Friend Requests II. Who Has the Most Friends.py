fc3f95fe16e572f65387bcd837cad92f489d4248
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-11-18 09:06:57 +0200

    602 solution

--- 06 - Subqueries/602. Friend Requests II. Who Has the Most Friends.py
+++ 06 - Subqueries/602. Friend Requests II. Who Has the Most Friends.py
@@ -1,1 +1,12 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def most_friends(request_accepted: pd.DataFrame) -> pd.DataFrame:
+    result = (
+        pd.concat([request_accepted["requester_id"], request_accepted["accepter_id"]])
+        .value_counts()
+        .reset_index(name="num")
+        .rename(columns={"index": "id"})
+        .head(1)
+    )
+    return result
