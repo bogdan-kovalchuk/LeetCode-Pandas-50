ee0d58cdc5ef1033f121b139df6e119720a50e25
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-11-27 14:30:26 +0200

    185 solution

--- 06 - Subqueries/185. Department Top Three Salaries.py
+++ 06 - Subqueries/185. Department Top Three Salaries.py
@@ -1,1 +1,29 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    employee = employee.merge(
+        department,
+        how="left",
+        left_on="departmentId",
+        right_on="id",
+        suffixes=("_emp", "_dep"),
+    )
+
+    employee = employee.rename(
+        columns={
+            "id_emp": "id",
+            "name_emp": "Employee",
+            "salary": "Salary",
+            "name_dep": "Department",
+        }
+    )
+
+    res = (
+        employee.groupby("Department")["Salary"]
+        .apply(lambda s: s.sort_values(ascending=False).unique()[:3])
+        .explode()
+        .reset_index(name="Salary")
+    )
+
+    return res.merge(employee, how="left")[["Department", "Employee", "Salary"]]
