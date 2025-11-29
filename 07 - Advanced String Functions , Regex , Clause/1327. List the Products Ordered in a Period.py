5271226b5a013128850bad87cb56223c16aa4944
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-11-29 16:14:09 +0200

    1327 solution

--- 07 - Advanced String Functions , Regex , Clause/1327. List the Products Ordered in a Period.py
+++ 07 - Advanced String Functions , Regex , Clause/1327. List the Products Ordered in a Period.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
 
-def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    return patients[patients["conditions"].str.contains(r"(^DIAB1| DIAB1)")]
+def list_products(products: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    orders = orders[(orders["order_date"].dt.year == 2020) & (orders["order_date"].dt.month == 2)]
+    orders = orders.groupby("product_id")["unit"].sum().reset_index()
+    orders = orders[orders["unit"] >= 100]
+    return orders.merge(products, how="left", on="product_id")[["product_name", "unit"]]
