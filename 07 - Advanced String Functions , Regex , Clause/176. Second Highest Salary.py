059b2ad034b6dbb16377c6ccfb904469ee79bd5a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-11-30 15:11:38 +0200

    176 solution

--- 07 - Advanced String Functions , Regex , Clause/176. Second Highest Salary.py
+++ 07 - Advanced String Functions , Regex , Clause/176. Second Highest Salary.py
@@ -1,1 +1,11 @@
 # TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    sec_salary = None
+    employee = employee.drop_duplicates(subset="salary")
+    if employee.shape[0] >= 2:
+        employee.sort_values(by="salary", ascending=False, inplace=True)
+        sec_salary = employee["salary"].iloc[1]
+    return pd.DataFrame({"SecondHighestSalary": [sec_salary]})
