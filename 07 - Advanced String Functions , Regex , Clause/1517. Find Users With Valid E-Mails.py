02dc312365f2826a4a0bd923a62434213fa2d82a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-12-01 10:40:40 +0200

    1517 solution

--- 07 - Advanced String Functions , Regex , Clause/1517. Find Users With Valid E-Mails.py
+++ 07 - Advanced String Functions , Regex , Clause/1517. Find Users With Valid E-Mails.py
@@ -1,1 +1,5 @@
-# TYPE YOUR CODE HERE
+import pandas as pd
+
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    return users[users["mail"].str.match(r"^[A-Za-z][A-Za-z0-9_.-]*@leetcode\.com$")]
